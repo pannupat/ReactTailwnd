a2c4618b0a8b009fee65a29c7b5dcf7adf01061e
Panupat Phonyiam <127047015+pannupat@users.noreply.github.com>
2024-04-23 16:38:25 +0700

    [Edit]:Tailwind<div>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import WsCard from "./Tailwind/CardFlip";
 
 function App() {
-  return (
-    <div className="flex flex-col overflow-auto items-center body h-screen  py-40 gap-8 back-card bg-hero ">
-      <WsCard />
-    </div>
-  );
+  return <WsCard />;
 }
 
 export default App;
--- src/Tailwind/CardFlip.tsx
+++ src/Tailwind/CardFlip.tsx
@@ -1,6 +1,10 @@
 import Newcard from "./FlipCard";
 
 const CardFlip = () => {
-  return <Newcard />;
+  return (
+    <div className="flex flex-col overflow-auto items-center body h-screen  py-[140px] gap-8 back-card bg-hero ">
+      <Newcard />;
+    </div>
+  );
 };
 export default CardFlip;
